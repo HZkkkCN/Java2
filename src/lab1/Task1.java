4e1bda7800a40a9a6fa50c035e88df29393c4657
HZkkkCN <986659645@qq.com>
2024-09-10 11:54:44 +0800

    first version

---
+++ src/lab1/Task1.java
@@ -0,0 +1,64 @@
+package lab1;
+
+import java.util.ArrayList;
+
+public class Task1 {
+    public static void main(String[] args) {
+        EmployeeManagementSystem system1=new EmployeeManagementSystem();
+        EmployeeManagementSystem.Employee A=new EmployeeManagementSystem.Employee("A",23);
+        EmployeeManagementSystem.Employee B=new EmployeeManagementSystem.Employee("B",24);
+        EmployeeManagementSystem.Employee C=new EmployeeManagementSystem.Employee("C",25);
+        EmployeeManagementSystem.Employee D=new EmployeeManagementSystem.Employee("D",26);
+        system1.addEmployee(A);
+        system1.addEmployee(B);
+        system1.addEmployee(C);
+        system1.deleteEmployee(B);
+        system1.addEmployee(D);
+        system1.addEmployee(A);
+        system1.getEmployees();
+    }
+
+    public static class EmployeeManagementSystem {
+        private ArrayList<Employee> Employees;
+        public EmployeeManagementSystem(){
+            this.Employees=Employees=new ArrayList<Employee>();
+        }
+
+        public void addEmployee(Employee e){
+            if(!Employees.contains(e)){
+                Employees.add(e);
+            }
+        }
+
+        public void deleteEmployee(Employee e){
+            if(Employees.contains(e)){
+                Employees.remove(e);
+            }
+        }
+
+        public void getEmployees(){
+            for (Employee e:
+                    Employees) {
+                System.out.println("Name: "+e.Name+"  "+"Age: "+e.age);
+            }
+        }
+
+        public static class Employee{
+            private String Name;
+            private int age;
+            public Employee(String Name,int age){
+                this.Name=Name;
+                this.age=age;
+            }
+
+            public String getName() {
+                return Name;
+            }
+            public int getAge(){
+                return age;
+            }
+        }
+
+
+    }
+}
